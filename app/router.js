f8dfdddf6d326503264dff0a37a7c2c1c1da21db
Ben Fischer <bfischer@enova.com>
2016-06-03 13:45:30 -0500

    a bit of app routing logic

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('inspections');
+  this.route('inspections', function() {
+    this.route('chart');
+  });
 });
 
 export default Router;
---
+++ tests/unit/routes/inspections/index-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:inspections/index', 'Unit | Route | inspections/index', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
